0708f31d39665e4c4b624ed52a353000492f978f
Nicola Buescher <nicolalucasbuescher@gmail.com>
2019-02-03 21:33:18 -0500

    declare timestamp as actual on linux

--- engine/src/linuxMain/kotlin/hazel/logging/Log.kt
+++ engine/src/linuxMain/kotlin/hazel/logging/Log.kt
@@ -8,11 +8,11 @@ import platform.posix.time_tVar
 
 
 @ExperimentalUnsignedTypes
-internal val timestamp: String
+internal actual val timestamp: String
     get() = memScoped {
         val t = alloc<time_tVar>()
         val str = allocArray<ByteVar>(10)
         time(t.ptr)
-        strftime(str, 10, "%H:%M:%S", localtime(t.ptr))
+        strftime(str, 10u, "%H:%M:%S", localtime(t.ptr))
         str.toKString()
     }
