e111986bc19091a8f4afeebf907b1ef7d2bdffd7
Nicola Buescher <nicolalucasbuescher@gmail.com>
2019-10-10 07:38:19 -0400

    our first triangle!!

--- hazel/src/nativeMain/kotlin/hazel/Application.kt
+++ hazel/src/nativeMain/kotlin/hazel/Application.kt
@@ -1,15 +1,78 @@
 package hazel
 
+import copengl.GL_ARRAY_BUFFER
 import copengl.GL_COLOR_BUFFER_BIT
+import copengl.GL_ELEMENT_ARRAY_BUFFER
+import copengl.GL_FALSE
+import copengl.GL_FLOAT
+import copengl.GL_STATIC_DRAW
+import copengl.GL_TRIANGLES
+import copengl.GL_UNSIGNED_INT
+import copengl.glBindBuffer
+import copengl.glBindVertexArray
+import copengl.glBufferData
 import copengl.glClear
 import copengl.glClearColor
+import copengl.glDrawElements
+import copengl.glEnableVertexAttribArray
+import copengl.glGenBuffer
+import copengl.glGenVertexArray
+import copengl.glVertexAttribPointer
+import kotlinx.cinterop.Arena
+import kotlinx.cinterop.FloatVar
+import kotlinx.cinterop.cValuesOf
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.invoke
+import kotlinx.cinterop.placeTo
+import kotlinx.cinterop.sizeOf
 
 abstract class Application {
+    private val scope = Arena()
+
     val window = Window().apply { setEventCallback(::onEvent) }
 
     private var isRunning: Boolean = true
     private val layerStack = LayerStack()
     private val imGuiLayer = ImGuiLayer()
+
+    private var vertexArray: UInt
+    private var vertexBuffer: UInt
+    private var indexBuffer: UInt
+
+    init {
+        vertexArray = glGenVertexArray()
+        glBindVertexArray!!(vertexArray)
+
+        vertexBuffer = glGenBuffer()
+        glBindBuffer!!(GL_ARRAY_BUFFER.convert(), vertexBuffer)
+
+        val vertices = cValuesOf(
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f,
+            0.0f, 0.5f, 0f
+        )
+
+        glBufferData!!(
+            GL_ARRAY_BUFFER.convert(),
+            vertices.size.convert(),
+            vertices.placeTo(scope),
+            GL_STATIC_DRAW.convert()
+        )
+
+        glEnableVertexAttribArray!!(0u)
+        glVertexAttribPointer!!(0u, 3, GL_FLOAT.convert(), GL_FALSE.convert(), (3 * sizeOf<FloatVar>()).convert(), null)
+
+        indexBuffer = glGenBuffer()
+        glBindBuffer!!(GL_ELEMENT_ARRAY_BUFFER.convert(), indexBuffer)
+
+        val indices = cValuesOf(0u, 1u, 2u)
+        glBufferData!!(
+            GL_ELEMENT_ARRAY_BUFFER.convert(),
+            indices.size.convert(),
+            indices.placeTo(scope),
+            GL_STATIC_DRAW.convert()
+        )
+    }
 
 
     fun addLayer(layer: Layer) {
@@ -28,8 +91,11 @@ abstract class Application {
         addOverlay(imGuiLayer)
 
         while (isRunning) {
-            glClearColor(1f, 0f, 1f, 1f)
+            glClearColor(0.1f, 0.1f, 0.1f, 1f)
             glClear(GL_COLOR_BUFFER_BIT)
+
+            glBindVertexArray!!(vertexArray)
+            glDrawElements(GL_TRIANGLES.convert(), 3, GL_UNSIGNED_INT.convert(), null)
 
             layerStack.forEach { it.onUpdate() }
 
@@ -39,6 +105,8 @@ abstract class Application {
 
             window.onUpdate()
         }
+
+        scope.clear()
     }
 
 
--- hazel/src/nativeMain/kotlin/hazel/renderer/OpenGLContext.kt
+++ hazel/src/nativeMain/kotlin/hazel/renderer/OpenGLContext.kt
@@ -3,16 +3,36 @@ package hazel.renderer
 import cglfw.glfwMakeContextCurrent
 import cglfw.glfwSwapBuffers
 import cnames.structs.GLFWwindow
+import copengl.GL_RENDERER
+import copengl.GL_VENDOR
+import copengl.GL_VERSION
+import copengl.glGetString
 import copengl.glewInit
+import hazel.Hazel
+import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.UByteVar
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.toKString
 
 class OpenGLContext(
     private val windowHandle: CPointer<GLFWwindow>
 ) : GraphicsContext {
 
+    private fun CPointer<UByteVar>.toKString() = reinterpret<ByteVar>().toKString()
+
     override fun init() {
         glfwMakeContextCurrent(windowHandle)
         glewInit()
+
+        Hazel.coreInfo {
+            """
+            OpenGL Info:
+              Vendor: ${glGetString(GL_VENDOR)?.toKString()}
+              Renderer: ${glGetString(GL_RENDERER)?.toKString()}
+              Version: ${glGetString(GL_VERSION)?.toKString()}
+            """.trimIndent()
+        }
     }
 
     override fun swapBuffers() {
