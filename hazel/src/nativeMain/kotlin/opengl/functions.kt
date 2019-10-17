8e049f1979b4bab283ab60d49c34115f4990c233
Nicola Buescher <nicolalucasbuescher@gmail.com>
2019-10-17 19:46:31 -0400

    make all gl functions internal

--- hazel/src/nativeMain/kotlin/opengl/functions.kt
+++ hazel/src/nativeMain/kotlin/opengl/functions.kt
@@ -27,94 +27,94 @@ import kotlinx.cinterop.value
 
 // A
 
-inline fun glAttachShader(program: UInt, shader: UInt) = copengl.glAttachShader!!(program, shader)
+internal inline fun glAttachShader(program: UInt, shader: UInt) = copengl.glAttachShader!!(program, shader)
 
 
 // B
 
-inline fun glBindBuffer(target: Int, buffer: UInt) = copengl.glBindBuffer!!(target.convert(), buffer)
+internal inline fun glBindBuffer(target: Int, buffer: UInt) = copengl.glBindBuffer!!(target.convert(), buffer)
 
-inline fun glBindVertexArray(array: UInt) = copengl.glBindVertexArray!!(array)
+internal inline fun glBindVertexArray(array: UInt) = copengl.glBindVertexArray!!(array)
 
-inline fun glBufferData(target: Int, data: FloatArray, usage: Int) = data.usePinned {
+internal inline fun glBufferData(target: Int, data: FloatArray, usage: Int) = data.usePinned {
     copengl.glBufferData!!(target.convert(), data.size * sizeOf<FloatVar>(), it.addressOf(0), usage.convert())
 }
 
-inline fun glBufferData(target: Int, data: UIntArray, usage: Int) = data.usePinned {
+internal inline fun glBufferData(target: Int, data: UIntArray, usage: Int) = data.usePinned {
     copengl.glBufferData!!(target.convert(), data.size * sizeOf<UIntVar>(), it.addressOf(0), usage.convert())
 }
 
 
 // C
 
-inline fun glClear(mask: Int) = copengl.glClear(mask.convert())
+internal inline fun glClear(mask: Int) = copengl.glClear(mask.convert())
 
-inline fun glCompileShader(shader: UInt) = copengl.glCompileShader!!(shader)
+internal inline fun glCompileShader(shader: UInt) = copengl.glCompileShader!!(shader)
 
-inline fun glCreateBuffer() = glCreateBuffers(1).first()
+internal inline fun glCreateBuffer() = glCreateBuffers(1).first()
 
-inline fun glCreateBuffers(n: Int) = UIntArray(n).apply {
+internal inline fun glCreateBuffers(n: Int) = UIntArray(n).apply {
     usePinned { copengl.glCreateBuffers!!(n, it.addressOf(0)) }
 }
 
-inline fun glCreateProgram() = copengl.glCreateProgram!!()
+internal inline fun glCreateProgram() = copengl.glCreateProgram!!()
 
-inline fun glCreateShader(shaderType: Int): UInt = copengl.glCreateShader!!(shaderType.convert())
+internal inline fun glCreateShader(shaderType: Int): UInt = copengl.glCreateShader!!(shaderType.convert())
 
-inline fun glCreateVertexArray() = glCreateVertexArrays(1).first()
+internal inline fun glCreateVertexArray() = glCreateVertexArrays(1).first()
 
-inline fun glCreateVertexArrays(n: Int) = UIntArray(n).apply {
+internal inline fun glCreateVertexArrays(n: Int) = UIntArray(n).apply {
     usePinned { copengl.glCreateVertexArrays!!(n, it.addressOf(0)) }
 }
 
 
 // D
 
-inline fun glDeleteBuffer(buffer: UInt) = glDeleteBuffers(1, uintArrayOf(buffer))
+internal inline fun glDeleteBuffer(buffer: UInt) = glDeleteBuffers(1, uintArrayOf(buffer))
 
-inline fun glDeleteBuffers(n: Int, buffers: UIntArray) {
+internal inline fun glDeleteBuffers(n: Int, buffers: UIntArray) {
     buffers.usePinned { copengl.glDeleteBuffers!!(n, it.addressOf(0)) }
 }
 
-inline fun glDeleteProgram(program: UInt) = copengl.glDeleteProgram!!(program)
+internal inline fun glDeleteProgram(program: UInt) = copengl.glDeleteProgram!!(program)
 
-inline fun glDeleteShader(shader: UInt) = copengl.glDeleteShader!!(shader)
+internal inline fun glDeleteShader(shader: UInt) = copengl.glDeleteShader!!(shader)
 
-inline fun glDeleteVertexArrays(vararg arrays: UInt) = arrays.usePinned {
+internal inline fun glDeleteVertexArrays(vararg arrays: UInt) = arrays.usePinned {
     copengl.glDeleteVertexArrays!!(arrays.size, it.addressOf(0))
 }
 
-inline fun glDetachShader(program: UInt, shader: UInt) = copengl.glDetachShader!!(program, shader)
+internal inline fun glDetachShader(program: UInt, shader: UInt) = copengl.glDetachShader!!(program, shader)
 
 
 // E
 
-inline fun glEnableVertexAttribArray(index: UInt) = copengl.glEnableVertexAttribArray!!(index)
+internal inline fun glEnableVertexAttribArray(index: UInt) = copengl.glEnableVertexAttribArray!!(index)
 
 
 // G
 
-inline fun glGetProgramInfoLog(program: UInt): String {
+internal inline fun glGetProgramInfoLog(program: UInt): String {
     val maxLength = glGetProgramiv(program, GL_INFO_LOG_LENGTH)
     return ByteArray(maxLength).apply {
         usePinned { copengl.glGetProgramInfoLog!!(program, maxLength, null, it.addressOf(0)) }
     }.toKString()
 }
 
-inline fun glGetProgramiv(program: UInt, pname: Int) = memScoped {
+internal inline fun glGetProgramiv(program: UInt, pname: Int) = memScoped {
     val iv = alloc<IntVar>()
     copengl.glGetProgramiv!!(program, pname.convert(), iv.ptr)
     iv.value
 }
 
-inline fun glGetShaderInfoLog(shader: UInt): String {
+internal inline fun glGetShaderInfoLog(shader: UInt): String {
     val maxLength = glGetShaderiv(shader, GL_INFO_LOG_LENGTH)
     return ByteArray(maxLength).apply {
         usePinned { copengl.glGetShaderInfoLog!!(shader, maxLength, null, it.addressOf(0)) }
     }.toKString()
 }
 
-inline fun glGetShaderiv(shader: UInt, pname: Int): Int = memScoped {
+internal inline fun glGetShaderiv(shader: UInt, pname: Int): Int = memScoped {
     val iv = alloc<IntVar>()
     copengl.glGetShaderiv!!(shader, pname.convert(), iv.ptr)
     iv.value
@@ -123,12 +123,12 @@ inline fun glGetShaderiv(shader: UInt, pname: Int): Int = memScoped {
 
 // L
 
-inline fun glLinkProgram(program: UInt) = copengl.glLinkProgram!!(program)
+internal inline fun glLinkProgram(program: UInt) = copengl.glLinkProgram!!(program)
 
 
 // S
 
-inline fun glShaderSource(shader: UInt, string: String) = memScoped {
+internal inline fun glShaderSource(shader: UInt, string: String) = memScoped {
     val source = alloc<CPointerVar<ByteVar>>()
     source.value = string.cstr.ptr
     copengl.glShaderSource!!(shader, 1, source.ptr, 0L.toCPointer())
@@ -137,12 +137,12 @@ inline fun glShaderSource(shader: UInt, string: String) = memScoped {
 
 // U
 
-inline fun glUseProgram(program: UInt) = copengl.glUseProgram!!(program)
+internal inline fun glUseProgram(program: UInt) = copengl.glUseProgram!!(program)
 
 
 // V
 
-inline fun glVertexAttribPointer(index: UInt, size: Int, type: Int, normalized: Boolean, stride: Int, pointer: Int) {
+internal inline fun glVertexAttribPointer(index: UInt, size: Int, type: Int, normalized: Boolean, stride: Int, pointer: Int) {
     copengl.glVertexAttribPointer!!(
         index,
         size,
