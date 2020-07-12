2fb61765e5688bcb4cecbea95e3e01b23f925407
Nicola Buescher <nicolalucasbuescher@gmail.com>
2020-07-11 22:44:31 -0400

    make logging easier

--- hazel/src/nativeMain/kotlin/hazel/core/Hazel.kt
+++ hazel/src/nativeMain/kotlin/hazel/core/Hazel.kt
@@ -57,26 +57,26 @@ fun Hazel.getTime(): Float = clock.elapsedNow().inSeconds.toFloat()
 private val coreLogger = Logger("HAZEL")
 private val clientLogger = Logger("APP")
 
-internal fun Hazel.coreTrace(message: String) = coreLogger.trace(message)
-internal fun Hazel.coreDebug(message: String) = coreLogger.debug(message)
-internal fun Hazel.coreInfo(message: String) = coreLogger.info(message)
-internal fun Hazel.coreWarn(message: String) = coreLogger.warn(message)
-internal fun Hazel.coreError(message: String) = coreLogger.error(message)
-internal fun Hazel.coreCritical(message: String) = coreLogger.critical(message)
-internal fun Hazel.coreAssert(test: Boolean, message: String? = null) {
+internal fun Hazel.coreTrace(message: Any) = coreLogger.trace(message)
+internal fun Hazel.coreDebug(message: Any) = coreLogger.debug(message)
+internal fun Hazel.coreInfo(message: Any) = coreLogger.info(message)
+internal fun Hazel.coreWarn(message: Any) = coreLogger.warn(message)
+internal fun Hazel.coreError(message: Any) = coreLogger.error(message)
+internal fun Hazel.coreCritical(message: Any) = coreLogger.critical(message)
+internal fun Hazel.coreAssert(test: Boolean, message: Any? = null) {
 	if (Hazel.Config.enableAsserts && !test) {
 		coreCritical("Assertion failed${message?.let { ": $it" } ?: ""}")
 		breakpoint()
 	}
 }
 
-fun Hazel.trace(message: String) = clientLogger.trace(message)
-fun Hazel.debug(message: String) = clientLogger.debug(message)
-fun Hazel.info(message: String) = clientLogger.info(message)
-fun Hazel.warn(message: String) = clientLogger.warn(message)
-fun Hazel.error(message: String) = clientLogger.error(message)
-fun Hazel.critical(message: String) = clientLogger.critical(message)
-fun Hazel.assert(test: Boolean, message: String? = null) {
+fun Hazel.trace(message: Any) = clientLogger.trace(message)
+fun Hazel.debug(message: Any) = clientLogger.debug(message)
+fun Hazel.info(message: Any) = clientLogger.info(message)
+fun Hazel.warn(message: Any) = clientLogger.warn(message)
+fun Hazel.error(message: Any) = clientLogger.error(message)
+fun Hazel.critical(message: Any) = clientLogger.critical(message)
+fun Hazel.assert(test: Boolean, message: Any? = null) {
 	if (Hazel.Config.enableAsserts && !test) {
 		critical("Assertion failed${message?.let { ": $it" } ?: ""}")
 		breakpoint()
